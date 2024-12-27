0a61e9dd0d748329eb11c040ddef7a3302044277
gambit328 <darklar@mail.ru>
2024-12-27 22:09:32 +0700

    27.12.2024 - gambit. delete hotkeys from fugitive

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -4,23 +4,20 @@ return {
 		config = function()
 			local map = vim.api.nvim_set_keymap
 
-			-- Просмотр статуса репозитория
-			map("n", "<leader>gs", ":Gstatus<CR>", { noremap = true, silent = true })
+			-- Открывает статус репозитория
+			map("n", "<leader>gs", ":Git status<CR>", { noremap = true, silent = true })
 
-			-- Переключение веток
-			map("n", "<leader>gb", ":Gbranch<CR>", { noremap = true, silent = true })
+			-- Открывает окно для коммита
+			map("n", "<leader>gc", ":Git commit<CR>", { noremap = true, silent = true })
 
-			-- Коммит
-			map("n", "<leader>gc", ":Gcommit<CR>", { noremap = true, silent = true })
+			-- Пуш изменений
+			map("n", "<leader>gp", ":Git push<CR>", { noremap = true, silent = true })
 
-			-- Просмотр изменений
-			map("n", "<leader>gd", ":Gdiff<CR>", { noremap = true, silent = true })
+			-- Показывает журнал изменений
+			map("n", "<leader>gl", ":Git log<CR>", { noremap = true, silent = true })
 
-			-- Просмотр истории коммитов
-			map("n", "<leader>gl", ":Glog<CR>", { noremap = true, silent = true })
-
-			-- Возврат к последнему коммиту
-			map("n", "<leader>gq", ":Greset --hard<CR>", { noremap = true, silent = true })
+			-- Показывает информацию о текущем коммите
+			map("n", "<leader>gb", ":Git blame<CR>", { noremap = true, silent = true })
 		end,
 	},
 }
